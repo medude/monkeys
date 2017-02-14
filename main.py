3aaaf64e12ab518b07f8ee87951257234053c100
medude <varner_nathan@yahoo.com>
2017-02-14 18:10:32 -0500

    comment out section
    that's all

--- main.py
+++ main.py
@@ -45,11 +45,6 @@ for i in range(neuron_layers_length):
 # training step
 for j in range(data["statistics"]["train_to_steps"]):
     # Calculate forward through the network.
-    # neuron_layers[0] = Layer(X, neuron_num=1, bias=1)
-    # neuron_layers[1] = Layer(nonlin(np.dot(neuron_layers[0].value, synapse_sets[0].value)),
-    #                          neuron_num=10)
-    # neuron_layers[2] = Layer(nonlin(np.dot(neuron_layers[1].value, synapse_sets[1].value)), is_output=True)
-
     for i in range(neuron_layers_length):
         value = X if i == 0 else nonlin(np.dot(neuron_layers[i - 1].value, synapse_sets[i - 1].value))
 
@@ -58,10 +53,10 @@ for j in range(data["statistics"]["train_to_steps"]):
                                  is_output=(i + 1 == neuron_layers_length))
 
     # Backpropagation of errors using the chain rule.
-    neuron_layers[2].calc_error(y)
-    neuron_layers[2].calc_delta()
-    neuron_layers[1].calc_error(neuron_layers[2], synapse_sets[1])
-    neuron_layers[1].calc_delta()
+    # neuron_layers[2].calc_error(y)
+    # neuron_layers[2].calc_delta()
+    # neuron_layers[1].calc_error(neuron_layers[2], synapse_sets[1])
+    # neuron_layers[1].calc_delta()
 
     # Sometimes print out the error
     if (j % data["statistics"]["record_error_every"]) == 0:
